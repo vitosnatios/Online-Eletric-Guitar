b3ef3343a042019913b0c325e4c5e46596e1bae1
vitosnatios <82514945+vitosnatios@users.noreply.github.com>
2022-03-18 13:59:06 -0300

    Code updated
    The index.html and app.js now have less repetition and the code is a lot smaller, what always SOUNDS good haha get it

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+var keyArray = ["1","2","3","4","5","6","7","Q","W","E","R","T","Y","U","A","S","D","F","G","H","J","Z","X","C","V","B","N","M"];
+keyArray.forEach(function(cadaTecla){
+    $("h4").before('<button class="a'+cadaTecla+' guitar btn btn-outline-dark">'+cadaTecla+'</button>');
+});
 for(var i=0; i < 28; i++){
     document.querySelectorAll("button")[i].addEventListener("click", function handleClick() {
     makeSound("a"+this.innerHTML);//this "a"+ makes possible to number keys to have animation, since it isnt possible to have a ".1" css style for a 'class="1"', for example
@@ -7,124 +10,17 @@ for(var i=0; i < 28; i++){
 document.addEventListener("keypress", function(event){
     makeSound("a"+event.key)//same here
     buttonsAnimation("a"+event.key)
-})
+});
 function makeSound (kiSorte) {
-
-    switch (kiSorte) { 
-        case "aZ":
-            var tom1 = new Audio("sounds/Z.wav")
-            tom1.play();
-            break;
-        case "aX":
-            var tom2 = new Audio("sounds/X.wav")
-            tom2.play();
-            break;
-        case "aC":
-            var tom3 = new Audio("sounds/C.wav")
-            tom3.play();
-            break;
-        case "aV":
-            var tom4 = new Audio("sounds/V.wav")
-            tom4.play();
-            break;
-        case "aB":
-            var tom5 = new Audio("sounds/B.wav")
-            tom5.play();
-            break;
-        case "aN":
-            var tom6 = new Audio("sounds/N.wav")
-            tom6.play();
-            break;
-        case "aM":
-            var tom7 = new Audio("sounds/M.wav")
-            tom7.play();
-            break;
-        case "aA":
-            var tom8 = new Audio("sounds/A.wav")
-            tom8.play();
-            break;
-        case "aS":
-            var tom9 = new Audio("sounds/S.wav")
-            tom9.play();
-            break;
-        case "aD":
-            var tom10 = new Audio("sounds/D.wav")
-            tom10.play();
-            break;
-        case "aF":
-            var tom11 = new Audio("sounds/F.wav")
-            tom11.play();
-            break;
-        case "aG":
-            var tom12 = new Audio("sounds/G.wav")
-            tom12.play();
-            break;
-        case "aH":
-            var tom13 = new Audio("sounds/H.wav")
-            tom13.play();
-            break;
-        case "aJ":
-            var tom14 = new Audio("sounds/J.wav")
-            tom14.play();
-            break;
-        case "aQ":
-            var tom15 = new Audio("sounds/Q.wav")
-            tom15.play();
-            break;
-        case "aW":
-            var tom16 = new Audio("sounds/W.wav")
-            tom16.play();
-            break;
-        case "aE":
-            var tom17 = new Audio("sounds/E.wav")
-            tom17.play();
-            break;
-        case "aR":
-            var tom18 = new Audio("sounds/R.wav")
-            tom18.play();
-            break;
-        case "aT":
-            var tom19 = new Audio("sounds/T.wav")
-            tom19.play();
-            break;
-        case "aY":
-            var tom20 = new Audio("sounds/Y.wav")
-            tom20.play();
-            break;
-        case "aU":
-            var tom21 = new Audio("sounds/U.wav")
-            tom21.play();
-            break;
-        case "a1":
-            var tom23 = new Audio("sounds/1.wav")
-            tom23.play();
-            break;
-        case "a2":
-            var tom24 = new Audio("sounds/2.wav")
-            tom24.play();
-            break;
-        case "a3":
-            var tom25 = new Audio("sounds/3.wav")
-            tom25.play();
-            break;
-        case "a4":
-            var tom26 = new Audio("sounds/4.wav")
-            tom26.play();
-            break;
-        case "a5":
-            var tom27 = new Audio("sounds/5.wav")
-            tom27.play();
-            break;
-        case "a6":
-            var tom28 = new Audio("sounds/6.wav")
-            tom28.play();
-            break;
-        case "a7":
-            var tom29 = new Audio("sounds/7.wav")
-            tom29.play();
-            break;
-        default: console.log(kiSorte);
-    }
+    let soundArray = ["Z","X","C","V","B","N","M","A","S","D","F","G","H","J","Q","W","E","R","T","Y","U","1","2","3","4","5","6","7"];
+    soundArray.forEach(function(cadaArray){
+    switch (kiSorte) {         
+            case "a"+cadaArray:
+            let tom = new Audio("sounds/"+cadaArray+".wav");
+            tom.play();
+            break;  
+        }
+    });
 }
 function buttonsAnimation(luckyKey){
     document.querySelector("."+luckyKey).classList.add("pressed");
