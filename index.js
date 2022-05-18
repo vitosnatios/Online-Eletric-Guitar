65f9f7b9eec0e07d5bdcc3b5599c7751641796ab
Vitor Fernandes <82514945+vitosnatios@users.noreply.github.com>
2022-05-18 02:15:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var keyArray = ["1","2","3","4","5","6","7","Q","W","E","R","T","Y","U","A","S","D","F","G","H","J","Z","X","C","V","B","N","M"];
-keyArray.forEach(function(cadaTecla){
+keyArray.forEach(function(cadaTecla, index){
+    if ((index+1) % 7 === 0 && index > 1 && index < 25){
+        $("h4").before('<button class="a'+cadaTecla+' guitar btn btn-outline-dark">'+cadaTecla+'</button></br>');
+    } else {
     $("h4").before('<button class="a'+cadaTecla+' guitar btn btn-outline-dark">'+cadaTecla+'</button>');
+    }
 });
 for(var i=0; i < 28; i++){
     document.querySelectorAll("button")[i].addEventListener("click", function handleClick() {
@@ -8,30 +12,39 @@ for(var i=0; i < 28; i++){
     buttonsAnimation("a"+this.innerHTML);
 });}
 document.addEventListener("keypress", function(event){
-    makeSound("a"+event.key)//same here
-    buttonsAnimation("a"+event.key)
+    makeSound("a"+event.key.toUpperCase())//same here
+    buttonsAnimation("a"+event.key.toUpperCase())
 });
+
+let distortion = false;
+function setDistortion(){
+    !distortion?distortion=true:distortion=false;
+}
+
 function makeSound (kiSorte) {
     let soundArray = ["Z","X","C","V","B","N","M","A","S","D","F","G","H","J","Q","W","E","R","T","Y","U","1","2","3","4","5","6","7"];
     soundArray.forEach(function(cadaArray){
-    switch (kiSorte) {         
+    if (soundArray.includes(kiSorte.slice(1))) {
+    switch (kiSorte) {    
             case "a"+cadaArray:
-            let tom = new Audio("sounds/"+cadaArray+".wav");
+            let tom = new Audio("sounds/"+cadaArray + (!distortion ? ".wav" : ".mp3") );
             tom.play();
-            break;  
-        }
-    });
+            tom.volume = 0.3;
+            break;
+        }}})
 }
 function buttonsAnimation(luckyKey){
+    if ( keyArray.includes(luckyKey.slice(1)) ){
     document.querySelector("."+luckyKey).classList.add("pressed");
     setTimeout (function(){
         document.querySelector("."+luckyKey).classList.remove("pressed");
     },200);
+    }
 }
 function playBackingTrack() {
-    var audio = new Audio("sounds/Cbackingtrack.mp3");
+    var audio = new Audio(!distortion ? "sounds/Cbackingtrack.mp3" : "sounds/rock_maligno.mp3");
     audio.play();
-    audio.volume = 0.3;
+    audio.volume = !distortion ? 0.3 : 0.14;
     }
 function stopBackingTrack(){
     window.location.reload();
